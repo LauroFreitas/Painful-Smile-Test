b8ee9877d469fe6214d554f32d384fdcb6521285
Lauro Henrique Santana de Freitas <lauroh.sfreitas@gmaill.com>
2020-09-11 15:11:03 -0300

    Assets do Projeto

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float maxVerticalSpeed;
+    public float maxRotationSpeed;
+    public float Speed;
+
+
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector3(0, Input.GetAxis("Vertical") * maxVerticalSpeed * Time.deltaTime));
+        transform.Rotate(new Vector3(0, 0, -1 * Input.GetAxis("Horizontal") * maxRotationSpeed * Time.deltaTime));
+    }
+}
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public float maxVerticalSpeed;
+    public GameObject bullets;
+    Transform frontalShoot;
+    Transform sideShoot;
+    Transform sideShoot2;
+    Transform sideShoot3;
+    private void Awake()
+    {
+         
+        frontalShoot = transform.GetChild(0).transform;
+        sideShoot = transform.GetChild(1).transform;
+        sideShoot2 = transform.GetChild(2).transform;
+        sideShoot3 = transform.GetChild(3).transform;
+         
+    }
+    private void Update()
+    {
+        Shoting();
+    }
+    public void Shoting() 
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GameObject a = Instantiate(bullets, frontalShoot.position, frontalShoot.rotation);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GameObject a = Instantiate(bullets, sideShoot.position, sideShoot.rotation);
+            GameObject b = Instantiate(bullets, sideShoot2.position, sideShoot2.rotation);
+            GameObject c = Instantiate(bullets, sideShoot3.position, sideShoot3.rotation);
+        }
+    }
+
+}
